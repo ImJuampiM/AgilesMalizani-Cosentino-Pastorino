bb64467bb2d3eedf8ff211e16ae60eedd0b8a778
lucio <luciocosen@gmail.com>
2026-06-22 21:32:56 -0300

    RED: UT adivinar letra presente revela todas sus ocurrencias

--- TP Integrador/tests/Ahorcado.test.ts
+++ TP Integrador/tests/Ahorcado.test.ts
@@ -10,3 +10,9 @@ it("una partida nueva empieza con 6 vidas", () => {
   const juego = new Ahorcado("GATO");
   expect(juego.vidasRestantes()).toBe(6);
 });
+
+it("adivinar una letra presente revela todas sus ocurrencias", () => {
+  const juego = new Ahorcado("ALA");
+  juego.adivinar("A");
+  expect(juego.palabraEnmascarada()).toBe("A _ A");
+});
