a36ebb51f0cdb644a2e87bd333a2f3559c994aca
lucio <luciocosen@gmail.com>
2026-06-22 21:33:13 -0300

    GREEN: adivinar revela todas las ocurrencias de la letra

--- TP Integrador/src/domain/Ahorcado.ts
+++ TP Integrador/src/domain/Ahorcado.ts
@@ -1,11 +1,20 @@
 export class Ahorcado {
+  private readonly adivinadas = new Set<string>();
+
   constructor(private readonly palabra: string) {}
 
   palabraEnmascarada(): string {
-    return this.palabra.split("").map(() => "_").join(" ");
+    return this.palabra
+      .split("")
+      .map((letra) => (this.adivinadas.has(letra) ? letra : "_"))
+      .join(" ");
   }
 
   vidasRestantes(): number {
     return 6;
+  }
+
+  adivinar(letra: string): void {
+    this.adivinadas.add(letra);
   }
 }
