d1cf6645b545781b75dba0c585b53f1e293346cf
lucio <luciocosen@gmail.com>
2026-06-22 21:28:40 -0300

    RED: UT partida nueva empieza con 6 vidas

--- TP Integrador/tests/Ahorcado.test.ts
+++ TP Integrador/tests/Ahorcado.test.ts
@@ -5,3 +5,8 @@ it("una partida nueva muestra la palabra enmascarada con guiones", () => {
   const juego = new Ahorcado("GATO");
   expect(juego.palabraEnmascarada()).toBe("_ _ _ _");
 });
+
+it("una partida nueva empieza con 6 vidas", () => {
+  const juego = new Ahorcado("GATO");
+  expect(juego.vidasRestantes()).toBe(6);
+});
