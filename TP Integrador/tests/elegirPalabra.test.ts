69312bc39944acbd994cfb0d956f9a19c466ca91
JuanPabloMalizani <jmalizani@frro.utn.edu.ar>
2026-06-29 20:58:38 -0300

    RED: UT elegirPalabra con rng cercano a 1 elige la ultima palabra

--- TP Integrador/tests/elegirPalabra.test.ts
+++ TP Integrador/tests/elegirPalabra.test.ts
@@ -5,3 +5,8 @@ it("elegirPalabra con un rng que devuelve 0 elige la primera palabra de la lista
   const lista = ["PERRO", "CABALLO", "ELEFANTE"];
   expect(elegirPalabra(lista, () => 0)).toBe("PERRO");
 });
+
+it("elegirPalabra con un rng cercano a 1 elige la última palabra de la lista", () => {
+  const lista = ["PERRO", "CABALLO", "ELEFANTE"];
+  expect(elegirPalabra(lista, () => 0.99)).toBe("ELEFANTE");
+});
