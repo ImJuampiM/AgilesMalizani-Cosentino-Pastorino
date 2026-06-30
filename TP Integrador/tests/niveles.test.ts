84e63313d6e57b2ca8c0f6a4466dd5f7b62a3714
lucio <luciocosen@gmail.com>
2026-06-30 15:53:52 -0300

    TEST: documentar tabla de niveles (facil 8 y default normal 6, ramas ya verdes)

--- TP Integrador/tests/niveles.test.ts
+++ TP Integrador/tests/niveles.test.ts
@@ -4,3 +4,11 @@ import { vidasDeNivel } from "../src/domain/niveles";
 it("el nivel dificil arranca con 4 vidas", () => {
   expect(vidasDeNivel("dificil")).toBe(4);
 });
+
+it("el nivel facil arranca con 8 vidas", () => {
+  expect(vidasDeNivel("facil")).toBe(8);
+});
+
+it("un nivel desconocido cae en las 6 vidas del nivel normal", () => {
+  expect(vidasDeNivel("imposible")).toBe(6);
+});
