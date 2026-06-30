07fa55d3da75f7bcafed0c5fefb815079e628e50
lucio <luciocosen@gmail.com>
2026-06-30 15:31:20 -0300

    RED: UT nuevaPartida reemplaza la partida en curso por una en limpio

--- TP Integrador/tests/Sesion.test.ts
+++ TP Integrador/tests/Sesion.test.ts
@@ -6,3 +6,11 @@ it("una sesion nueva tiene una partida en curso con la palabra enmascarada y 6 v
   expect(sesion.partidaActual().palabraEnmascarada()).toBe("_ _ _ _");
   expect(sesion.partidaActual().vidasRestantes()).toBe(6);
 });
+
+it("nuevaPartida reemplaza la partida en curso por una en limpio", () => {
+  const sesion = new Sesion(["GATO"], () => 0);
+  sesion.partidaActual().adivinar("G");
+  sesion.nuevaPartida();
+  expect(sesion.partidaActual().palabraEnmascarada()).toBe("_ _ _ _");
+  expect(sesion.partidaActual().vidasRestantes()).toBe(6);
+});
